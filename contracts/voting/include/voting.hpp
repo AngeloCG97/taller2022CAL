421b7ec6e8b5b8e2e7c320b4242d7f4e2c35de12
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-08-27 17:25:00 -0600

    feat(contracts): add structure

---
+++ contracts/voting/include/voting.hpp
@@ -0,0 +1,12 @@
+#include <eosio/eosio.hpp>
+using namespace eosio;
+
+CONTRACT voting : public contract
+{
+public:
+   using contract::contract;
+
+   ACTION hi(name nm);
+
+   using hi_action = action_wrapper<"hi"_n, &voting::hi>;
+};
---
+++ contracts/voting/src/voting.cpp
@@ -0,0 +1,5 @@
+#include <voting.hpp>
+ACTION voting::hi( name nm ) {
+   /* fill in action body */
+   print_f("Name : %\n",nm);
+}
