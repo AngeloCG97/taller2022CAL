1c4ee0ecf756cd1faf5581080ae08bca8ed3a920
Angelo Castro Gamboa <51149817+AngeloCG97@users.noreply.github.com>
2022-08-27 15:36:40 -0600

    feat(webapp): create vote component (#1)

--- webapp/src/routes/Home/index.js
+++ webapp/src/routes/Home/index.js
@@ -1,15 +1,33 @@
 import React, { memo } from 'react'
 import { useTranslation } from 'react-i18next'
-import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+
+import VoteItem from '../../components/VoteItem'
+import { Grid } from '@mui/material'
+
+const options = [
+  {
+    name: 'Introducción a Blockchain',
+    image: 'images/bc-image.png',
+    alt: 'Introducción a Blockchain image'
+  },
+  {
+    name: 'Introducción a la Inteligenci Artificial',
+    image: 'images/ia-image.jpg',
+    alt: 'Introducción a la Inteligenci Artificial'
+  }
+]
 
 const Home = () => {
   const { t } = useTranslation('homeRoute')
 
   return (
-    <Box>
+    <Grid container>
       <Typography>{t('welcomeMessage')}</Typography>
-    </Box>
+      {options.map((option, index) => (
+        <VoteItem key={index} item={option} />
+      ))}
+    </Grid>
   )
 }
 
