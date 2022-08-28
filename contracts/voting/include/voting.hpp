4a2ccd48d02413e0bf295cfe3c3ecc9693dc53b0
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-08-27 18:10:52 -0600

    feat(contracts): add logic to vote

--- contracts/voting/include/voting.hpp
+++ contracts/voting/include/voting.hpp
@@ -1,4 +1,6 @@
 #include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+
 using namespace eosio;
 
 CONTRACT voting : public contract
@@ -6,7 +8,32 @@ CONTRACT voting : public contract
 public:
    using contract::contract;
 
-   ACTION hi(name nm);
+   ACTION vote(name voter, bool agree);
 
-   using hi_action = action_wrapper<"hi"_n, &voting::hi>;
+   [[eosio::on_notify("eosio.token::transfer")]] void onpay(name from, name to, asset quantity, std::string memo);
+
+private:
+   const int64_t REGISTRATION_COST = 3;
+
+   TABLE enroll
+   {
+      name student;
+
+      auto primary_key() const
+      {
+         return student.value;
+      }
+   };
+   typedef multi_index<name("enroll"), enroll> enroll_table;
+
+   TABLE votes
+   {
+      name voter;
+
+      auto primary_key() const
+      {
+         return voter.value;
+      }
+   };
+   typedef multi_index<name("votes"), votes> votes_table;
 };
--- contracts/voting/src/voting.cpp
+++ contracts/voting/src/voting.cpp
@@ -1,5 +1,30 @@
 #include <voting.hpp>
-ACTION voting::hi( name nm ) {
-   /* fill in action body */
-   print_f("Name : %\n",nm);
+
+ACTION voting::vote(name voter, bool agree)
+{
+   enroll_table _enroll{get_self(), get_self().value};
+   auto enroll_itr = _enroll.find(voter.value);
+
+   check(enroll_itr != _enroll.end(), "Student is not enrolled");
+
+   votes_table _votes{get_self(), get_self().value};
+   auto votes_itr = _votes.find(voter.value);
+
+   check(votes_itr != _votes.end(), "Student already voted");
+
+   _votes.emplace(get_self(), [&](auto &row)
+                  { row.voter = voter; });
+}
+
+ACTION voting::onpay(name from, name to, asset quantity, std::string memo)
+{
+   check(quantity.amount == REGISTRATION_COST, "The cost to enroll is incorrect, please pay " + std::to_string(REGISTRATION_COST) + " EOS only.");
+
+   enroll_table _enroll{get_self(), get_self().value};
+   auto enroll_itr = _enroll.find(from.value);
+
+   check(enroll_itr == _enroll.end(), "Student is already enrolled");
+
+   _enroll.emplace(get_self(), [&](auto &row)
+                   { row.student = from; });
 }
