6d98c396b30a3f81204ffb1d086135eb30bfcdb9
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-09-04 23:09:00 -0600

    fix(webapp): update contract to pay

--- webapp/src/config/main.config.js
+++ webapp/src/config/main.config.js
@@ -2,7 +2,7 @@ export const appVersion = process.env.REACT_APP_TAG || 'v1.0'
 export const name = process.env.REACT_APP_NAME
 export const title = process.env.REACT_APP_TITLE
 export const logo = process.env.REACT_APP_LOGO
-export const bloksExplorer = process.env.REAT_APP_BLOKS_EXPLORER
+export const bloksExplorer = process.env.REACT_APP_BLOKS_EXPLORER
 export const footerLinks = JSON.parse(
   process.env.REACT_APP_FOOTER_LINKS || '[]'
 )
